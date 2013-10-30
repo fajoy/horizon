9f24c4d32fb2327790c51060fcfd0b304d1002dc
fajoy <wuminfajoy@gmail.com>
2013-10-30 14:37:57 +0800

    fix session instances_ids_cache bug.

--- custom/hadoop/index.py
+++ custom/hadoop/index.py
@@ -355,13 +355,13 @@ class UpdateRow(tables.Row):
     def get_data(self, request,id):
         datum = hadoop.get_group_meta(request,id)
         datum["request"]= request
-        if not request.session.has_key("instances_cache"):
+        if not request.session.has_key("instances_ids_cache"):
             search_opts= {}
             instances,_more = api.nova.server_list(request,search_opts=search_opts )
             inst_ids_cache = [inst.id for inst in instances]
             request.session["instances_ids_cache"]= instances_ids_cache
         
-        inst_ids_cache = request.session["instances_cache"]
+        inst_ids_cache = request.session["instances_ids_cache"]
         meta_list= hadoop.get_instance_list(request,datum["hadoop_group_id"])
         datum["instance_count"]= str(len(meta_list))
         meta_ids= set( meta["uuid"] for meta in meta_list)
